e1cd6986ac7f5ca6169d83f4d51ef1b46507b3a7
xtof <christophe.alex@gmail.com>
2024-04-02 09:47:12 +0200

    Fix and coverage

--- test/NajaVerilogTest9.cpp
+++ test/NajaVerilogTest9.cpp
@@ -27,9 +27,42 @@ TEST(NajaVerilogTest9, test) {
   constructor.setFirstPass(true);
   constructor.parse(test9Path);
   ASSERT_EQ(1, constructor.modules_.size());
+  EXPECT_TRUE(constructor.modules_[0]->nets_.empty());
+  EXPECT_TRUE(constructor.modules_[0]->assigns_.empty());
+  EXPECT_TRUE(constructor.modules_[0]->instances_.empty());
   auto test = constructor.modules_[0];
+  const auto& ports = constructor.modules_[0]->ports_;
+  ASSERT_EQ(7, constructor.modules_[0]->ports_.size());
+  EXPECT_EQ(naja::verilog::Identifier("i1", false), ports[0].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("i2%", true), ports[1].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("i3", false), ports[2].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("o1", false), ports[3].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("o2", false), ports[4].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("io1", false), ports[5].identifier_);
+  EXPECT_EQ(naja::verilog::Identifier("io2", false), ports[6].identifier_);
+
+  EXPECT_EQ(naja::verilog::Port::Direction::Input, ports[0].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::Input, ports[1].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::Input, ports[2].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::Output, ports[3].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::Output, ports[4].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::InOut, ports[5].direction_);
+  EXPECT_EQ(naja::verilog::Port::Direction::InOut, ports[6].direction_);
+
+  EXPECT_FALSE(ports[0].range_.valid_);
+  EXPECT_FALSE(ports[1].range_.valid_);
+  EXPECT_FALSE(ports[2].range_.valid_);
+  EXPECT_TRUE(ports[3].range_.valid_);
+  EXPECT_EQ(3, ports[3].range_.msb_);
+  EXPECT_EQ(0, ports[3].range_.lsb_);
+  EXPECT_FALSE(ports[4].range_.valid_);
+  EXPECT_FALSE(ports[5].range_.valid_);
+  EXPECT_FALSE(ports[6].range_.valid_);
   
   constructor.setFirstPass(false);
   constructor.parse(test9Path);
   ASSERT_EQ(1, constructor.modules_.size());
+  EXPECT_TRUE(constructor.modules_[0]->nets_.empty());
+  EXPECT_TRUE(constructor.modules_[0]->assigns_.empty());
+  EXPECT_TRUE(constructor.modules_[0]->instances_.empty());
 }
